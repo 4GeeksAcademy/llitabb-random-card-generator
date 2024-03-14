1ea74bbbdeb0942f1253bb861f7c6d35dd86750e
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-03-14 17:24:09 +0000

    Add symbol and value to HTML

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 // PSEUDO CODIGO
@@ -10,12 +9,17 @@ import "./assets/img/4geeks.ico";
 // DONE - crear array para determinar el número (0-12)
 // DONE - generar numero aleatorio para palos
 // DONE - generar numero aleatorio para nÚmero
+// DONE - asociar al HTML para que aparezca en el navegador
 // asociar evento onload (ya creado)
 // estilizar
+
 window.onload = function() {
   let symbols = ["♦", "♥", "♠", "♣"];
   let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
   let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
   let randomValue = value[Math.floor(Math.random() * value.length)];
+
+  document.getElementById("symbol").innerHTML = randomSymbol;
+  document.getElementById("value").innerHTML = randomValue;
 };
