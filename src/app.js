6fde2bac8c73741fb0863e913603bfdcbc8699f6
Estrella Benítez Buitrago <156102986+Llitabb@users.noreply.github.com>
2024-03-14 20:54:51 +0000

    Entrega

--- src/app.js
+++ src/app.js
@@ -10,16 +10,23 @@ import "./assets/img/4geeks.ico";
 // DONE - generar numero aleatorio para palos
 // DONE - generar numero aleatorio para nÚmero
 // DONE - asociar al HTML para que aparezca en el navegador
-// asociar evento onload (ya creado)
-// estilizar
+// DONE - hacer que cuando aparezca el simbolo ♥ sea de color rojo
+// DONE - estilizar
 
 window.onload = function() {
   let symbols = ["♦", "♥", "♠", "♣"];
   let value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
 
-  let randomSymbol = symbols[Math.floor(Math.random() * symbols.length)];
-  let randomValue = value[Math.floor(Math.random() * value.length)];
+  let randomIndex = Math.floor(Math.random() * symbols.length);
+  let randomSymbol = symbols[randomIndex];
 
-  document.getElementById("symbol").innerHTML = randomSymbol;
-  document.getElementById("value").innerHTML = randomValue;
+  document.getElementById("symbol1").innerHTML = randomSymbol;
+  document.getElementById("symbol2").innerHTML = randomSymbol;
+  document.getElementById("value").innerHTML =
+    value[Math.floor(Math.random() * value.length)];
+
+  if (randomSymbol === "♥") {
+    document.getElementById("symbol1").classList.add("red-heart");
+    document.getElementById("symbol2").classList.add("red-heart");
+  }
 };
